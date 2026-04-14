556b52c23245de1288a8163e038c0f8a6937c59b
Vitor Honorato <vittorhonoratoo@gmail.com>
2026-04-14 20:27:32 -0300

    primeiro commit

---
+++ src/main/java/com/vittorhonorato/s3_crud/S3CrudApplication.java
@@ -0,0 +1,13 @@
+package com.vittorhonorato.s3_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3CrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vittorhonorato/s3_crud/S3CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vittorhonorato.s3_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
